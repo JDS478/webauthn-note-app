e10316fa817e7462f554857214aa4dbdbb4813b4
Joel Savage <joel28169@gmail.com>
2023-09-28 22:53:13 +0100

    Wenauthn origin setup with .env

---
+++ config/initializers/webauthn.rb
@@ -0,0 +1,8 @@
+WebAuthn.configure do |config|
+  # This value needs to match `window.location.origin` evaluated by
+  # the User Agent during registration and authentication ceremonies.
+  config.origin = ENV['WEBAUTHN_ORIGIN']
+
+  # Relying Party name for display purposes
+  config.rp_name = "Demo Webauthn App"
+end
