8543c17b4d7e0aec371ef0d0d0216e7e140b1386
Joel Savage <joel28169@gmail.com>
2023-10-19 22:36:26 +0100

    started setting up js and backend options

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -23,7 +23,48 @@ class RegistrationsController < ApplicationController
 
   def cred_login; end
 
-  def cred_callback; end
+  def user_callback
+    user = User.find_by(username: params[:username] )
+
+    return no_user_error unless user
+
+    options = WebAuthn::Credential.options_for_get(
+      allow: user.credentials.map { |cred| cred.external_id }
+    )
+    # Store the newly generated challenge somewhere so you can have it
+    # for the verification phase.
+    session[:authentication_challenge] = options.challenge
+
+    render json: options
+  end
+
+  def cred_callback
+    # Assuming you're using @github/webauthn-json package to send the `PublicKeyCredential` object back
+    # in params[:publicKeyCredential]:
+    webauthn_credential = WebAuthn::Credential.from_get(params[:publicKeyCredential])
+
+    credential = user.credentials.find_by(webauthn_id: webauthn_credential.id)
+
+    begin
+      webauthn_credential.verify(
+        session[:authentication_challenge],
+        public_key: credential.public_key,
+        sign_count: credential.sign_count
+      )
+
+      # Update the stored credential sign count with the value from `webauthn_credential.sign_count`
+      credential.update!(sign_count: webauthn_credential.sign_count)
+
+      # Continue with successful sign in or 2FA verification...
+
+    rescue WebAuthn::SignCountVerificationError => e
+      # Cryptographic verification of the authenticator data succeeded, but the signature counter was less then or equal
+      # to the stored value. This can have several reasons and depending on your risk tolerance you can choose to fail or
+      # pass authentication. For more information see https://www.w3.org/TR/webauthn/#sign-counter
+    rescue WebAuthn::Error => e
+      # Handle error
+    end
+  end
 
   private
 
@@ -33,5 +74,10 @@ class RegistrationsController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username)
+  end
+
+  def no_user_error
+    logger.warn 'No user found for given username'
+    render json: { error: 'No User found' }
   end
 end
--- app/javascript/packs/credential_login.js
+++ app/javascript/packs/credential_login.js
@@ -1,1 +1,12 @@
-export default () => {}
+export default () => {
+  if (document.getElementById('credential-login-btn')) {
+    document.querySelector('#credential-login-btn').addEventListener('click', async () => {
+      const username = document.querySelector('#username-input');
+
+      const userOptions = await getUserOptions(username);
+      // const loginCallbackUrl = '/dashboard/callback';
+
+      // create(loginCallbackUrl, userOptions);
+    })
+  }
+}
